dc2475265ccd2bb2741a53802427b519938c577a
Saquib Anjum <saquibanjum8969@gmail.com>
2025-03-10 01:38:21 +0530

    Backend Practice

--- Assignments/Backend Practice Questions/Q1 Solution/app.js
+++ Assignments/Backend Practice Questions/Q1 Solution/app.js
@@ -1,6 +1,9 @@
 import express from 'express';
  const app = express();
- app.use(/)
+ app.use(express.json())
+ app.get('/',(req,res)=>{
+res.send("Hello World")
+ })
  const PORT =3000;
  app.listen(PORT,()=>{
     console.log(`Server is  running on address http://localhost:${PORT}`)
