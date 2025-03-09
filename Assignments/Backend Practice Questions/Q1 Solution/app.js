7c638f0a752413f6a3c5ec9c86c43bf815ba7d86
Saquib Anjum <saquibanjum8969@gmail.com>
2025-03-10 01:34:24 +0530

    : :

---
+++ Assignments/Backend Practice Questions/Q1 Solution/app.js
@@ -0,0 +1,7 @@
+import express from 'express';
+ const app = express();
+ app.use(/)
+ const PORT =3000;
+ app.listen(PORT,()=>{
+    console.log(`Server is  running on address http://localhost:${PORT}`)
+ })
