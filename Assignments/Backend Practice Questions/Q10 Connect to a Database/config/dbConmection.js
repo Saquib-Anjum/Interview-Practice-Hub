5e2fb5508f45718d0b6c0c202fa10e4f29051525
Saquib Anjum <saquibanjum8969@gmail.com>
2025-03-25 21:15:54 +0530

    Q10

---
+++ Assignments/Backend Practice Questions/Q10 Connect to a Database/config/dbConmection.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userModel from '../models/userModel.js'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const dbConnection = async()=>{
+mongoose.connection.on('connected',()=>{
+    console.log("DB Connected")
+})
+await mongoose.connect(`${process.env.DB_URL}`)
+}
+export default dbConnection;
--- Assignments/Backend Practice Questions/Q10 Connect to a Database/server.js
+++ Assignments/Backend Practice Questions/Q10 Connect to a Database/server.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dbConnection from './config/dbConmection.js';
 const app = express();
-const PORT  = 3000;
+import dotenv from 'dotenv';
+dotenv.config();
+//config
+app.use(dbConnection);
+
+//api end point
+
+app.use(express.json())
+const PORT  = 4000;
 app.listen(PORT,()=>{
     console.log(` server is running on http://localhost:${PORT}`)
 })
