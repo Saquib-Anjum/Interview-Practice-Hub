95653c29e0f34a8bd919963191a9b8e6f3bfcba2
Saquib Anjum <saquibanjum8969@gmail.com>
2025-04-18 16:22:51 +0530

    Integrate Tailwind CSS into the project by updating package.json and vite.config.js, and remove unused App.css styles. Implement increment and decrement functionality in App.jsx for the counter app.

--- Assignments/Frontend 📚/20 frontend practice question solution/Q1 Countr with multiple features/src/App.jsx
+++ Assignments/Frontend 📚/20 frontend practice question solution/Q1 Countr with multiple features/src/App.jsx
@@ -5,29 +5,23 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const increment=()=>{
+    setCount(prev => prev + 1)
+  }
 
+  const decrement = ()=>{
+    setCount(prev => prev - 1)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className='text-center'>Cunter App </h1>
+    <div className="flex items-center justify-center">
+       <p className="text-red-500">{count}</p>
+       <div className="flex p-10 gap-10">
+        <button onClick={increment}>INCREMENT +</button>
+        <button  onClick={decrement}>DECREMENT -</button>
+       </div>
+    </div>
     </>
   )
 }
