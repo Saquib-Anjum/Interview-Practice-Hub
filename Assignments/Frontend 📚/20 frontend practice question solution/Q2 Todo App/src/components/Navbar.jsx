aeea7bac9e8cea5ab92e7ba5c6ee3a80411fbb5b
Saquib Anjum <saquibanjum8969@gmail.com>
2025-05-17 16:35:23 +0530

    Update project to integrate Tailwind CSS, modify index.html title, and implement Navbar component in App.jsx. Remove unused App.css styles and adjust package.json and package-lock.json for new dependencies.

---
+++ Assignments/Frontend 📚/20 frontend practice question solution/Q2 Todo App/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=' flex  items-center justify-between bg-clip-padding backdrop-filter backdrop-blur-lg   text-white bg-opacity-100'>
+      <img src="/logo.svg" alt=""  className=" w-10 ml-5 py-3" />
+      <div className="text-2xl font-semibold hidden sm:block">
+        <p className="text-green-500">Your Time is in Your Hand</p>
+      </div>
+      <div className='mr-5'>
+        <button className="bg-gray-900 py-3 px-2 gap-2 rounded-full"> ➕ ADD TODO</button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
