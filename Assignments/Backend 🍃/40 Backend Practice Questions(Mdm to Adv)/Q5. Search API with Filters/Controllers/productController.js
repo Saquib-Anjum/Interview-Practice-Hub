de84bcf52a566318dc48c11b95e83ed8ae16c493
Saquib Anjum <saquibanjum8969@gmail.com>
2025-05-20 23:08:41 +0530

    Add product search functionality in productController to fetch and return products based on category from external API.

--- Assignments/Backend 🍃/40 Backend Practice Questions(Mdm to Adv)/Q5. Search API with Filters/Controllers/productController.js
+++ Assignments/Backend 🍃/40 Backend Practice Questions(Mdm to Adv)/Q5. Search API with Filters/Controllers/productController.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import axios from 'axios';
 const productController = async( req , res)=>{
+const baseUrl = "https://dummyjson.com/products/search?q="
+const {category} = req.body;
+const apiData = await axios.get(`https://dummyjson.com/products/search?q=${category}`);
+const data = await apiData.data;
 
+res.json({
+   data
+})
 }
 export {productController};
